24d83594e9e7d048fe36286e89e4ee911679989a
djavo110 <mirzayevjavohir@example.com>
2025-10-01 01:03:00 +0500

    java

--- portfolium/forms.py
+++ portfolium/forms.py
@@ -46,10 +46,3 @@ class ContactForm(forms.ModelForm):
         fields = ['name', 'email', 'subject', 'message']
 
 
-class UserLoginForm(forms.Form):
-    username = forms.CharField(label='login', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Login'}))
-    password = forms.CharField(label='parol', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Parol'}))
-    captcha = CaptchaField(required=False)
-
-    class Meta:
-        fields = ['username', 'password', 'captcha']
